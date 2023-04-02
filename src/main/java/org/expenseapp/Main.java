0d648ddb4f8bfabeb681e86ea868f3845de3e80f
honeytalreja <talrejahoney8@gmail.com>
2023-04-02 21:02:08 +0530

    Initial code

--- src/main/java/org/expenseapp/Main.java
+++ src/main/java/org/expenseapp/Main.java
@@ -2,6 +2,6 @@ package org.expenseapp;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Development branch!");
     }
 }
